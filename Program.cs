33078fc0f66499ab9a219ee06a1a205fd220f42e
Charlie Wallace <charlescwallace2017@gmail.com>
2023-12-10 10:26:45 -0500

    Add functionality for number words wip

--- Program.cs
+++ Program.cs
@@ -3,26 +3,90 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
-
-
-var lines = System.IO.File.ReadAllLines(@"C:\Users\CharlieW\source\repos\advent_of_code_23\input.txt");
-
-List<string> LogList = new List<string>();
+// convert lines from string[] to List<string>
+var lines = System.IO.File.ReadAllLines(@"C:\Users\CharlieW\source\repos\advent_of_code_23\input2.txt");
+List<string> originalCodeList = new List<string>();
 
 foreach (var s in lines)
 {
-    LogList.Add(s);
+    originalCodeList.Add(s);
 }
 
+
+// functions to replace number words with digits
+static string findStringNum(string str)
+{
+    switch (str)
+    {
+        case "one":
+            return "1";
+        case "two":
+            return "2";
+        case "three":
+            return "3";
+        case "four":
+            return "4";
+        case "five":
+            return "5";
+        case "six":
+            return "6";
+        case "seven":
+            return "7";
+        case "eight":
+            return "8";
+        case "nine":
+            return "9";
+        default: return "";
+    }
+}
+
+List<string> testNumStrings = new List<string>();
+testNumStrings.Add("one");
+testNumStrings.Add("two");
+testNumStrings.Add("three");
+testNumStrings.Add("four");
+testNumStrings.Add("five");
+testNumStrings.Add("six");
+testNumStrings.Add("seven");
+testNumStrings.Add("eight");
+testNumStrings.Add("nine");
+
+static string replaceStringWithNumString(string codeString, List<string> stringNumList, List<string> newCodeList)
+{
+    for (int k = 0; k < stringNumList.Count; k++)
+    {
+        if (codeString.Contains(stringNumList[k]))
+        {
+            string num = stringNumList[k];
+            int index = codeString.IndexOf(num);
+            string cleanPath = (index < 0)
+                ? codeString
+                : codeString.Replace(num, findStringNum(num));
+            newCodeList.Add(cleanPath);
+        }
+    }
+
+    return "";
+}
+
+// number names are converted to digits in preparation of final summation
+List<string> newCodeList = new List<string>();
+
+for (int i = 0; i < originalCodeList.Count; i++)
+{
+    replaceStringWithNumString(originalCodeList[i], testNumStrings, newCodeList);
+}
+
+// newCodeList is looped through to count the first and last string digits and then sum them
 List<long> sumList = new List<long>();
 long totalSum = 0;
 
 try
 {
-    foreach (string inputString in LogList)
+    foreach (string inputString in newCodeList)
     {
-
         {
             List<char> lineNumsList = new List<char>();
 
@@ -32,7 +96,6 @@ try
 
                 if (char.IsDigit(c))
                 {
-
                     lineNumsList.Add(c);
                 }
             }
@@ -71,7 +134,7 @@ finally
 {
     sumList.ForEach(x => totalSum += x);
     Console.WriteLine(totalSum.ToString());
-    Console.WriteLine("Executing finally block.");
 }
+
 
 
