12d8659e32a3a85cd30c95e38f0abf472c697d18
Charlie Wallace <charlescwallace2017@gmail.com>
2023-12-13 21:08:58 -0500

    Getting there

--- Program.cs
+++ Program.cs
@@ -14,12 +14,14 @@ for (int i = 0; i < lines.Length; i++)
     {
         char l = line[j];
 
-        string stringNum = l.ToString();
+        string stringNum = "";
 
         int indexPointer = j;
 
         if (char.IsDigit(l))
         {
+            stringNum = l.ToString();
+
             char nextChar = line[indexPointer + 1];
 
             while (nextChar != '.' && nextChar != '\n')
@@ -31,31 +33,33 @@ for (int i = 0; i < lines.Length; i++)
                 nextChar = line[indexPointer + 1];
             }
 
-            int fullNum = int.Parse(stringNum);
+            int fullNum = 0;
+
+            if (int.TryParse(stringNum, out int result)) fullNum = int.Parse(stringNum);
 
             if (checkValidNum(fullNum, j, indexPointer, i, lines))
             {
                 totalSum += fullNum;
             }
+
+            j = indexPointer;
         }
-
-        j = indexPointer;
     }
-
-    Console.WriteLine(totalSum);
 }
+
+Console.WriteLine(totalSum);
 
 
 static bool checkValidNum(int num, int startIdx, int endIdx, int currentLineIdx, string[] lines)
 {
     string top = "";
-    if (currentLineIdx > 0) top = lines[currentLineIdx - 1].Substring(startIdx - 1, num.ToString().Length);
+    if (currentLineIdx > 0) top = lines[currentLineIdx - 1].Substring((startIdx - 1 < 0 ? startIdx : startIdx - 1), num.ToString().Length);
     string left = "";
     if (startIdx > 0) left = lines[currentLineIdx][startIdx - 1].ToString();
     string right = "";
     if ((endIdx + 1) < lines[currentLineIdx].Length) right = lines[currentLineIdx][endIdx + 1].ToString();
     string bottom = "";
-    if ((currentLineIdx + 1) <= (lines.Length - 1)) bottom = lines[currentLineIdx + 1].Substring((startIdx - 1 < 0 ? startIdx : startIdx - 1), num.ToString().Length);
+    if ((currentLineIdx + 1) < lines[currentLineIdx].Length) bottom = lines[currentLineIdx + 1].Substring((startIdx - 1 < 0 ? startIdx : startIdx - 1), num.ToString().Length + 1);
 
     StringBuilder sb = new StringBuilder();
     string checkStrings = sb.Append(top + left + right + bottom).ToString();
